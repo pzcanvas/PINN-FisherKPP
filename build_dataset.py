fixed data generation and added scaling layer for model

build_dataset.py
@@ -4,8 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imread
 
-# data type
-DTYPE = 'float32'
 
 # number of sampled points 
 N_INITIAL = 50
@@ -28,16 +26,11 @@ def u_0(x):
 def u_b(t, x):
 	return 1 / (1 + np.exp(x - 5 * t)) ** 2
 
+def get_bounds(t_min = T_MIN, t_max = T_MAX, x_min = X_MIN, x_max = X_MAX):
+	return t_min, t_max, x_min, x_max
 
-def generate_data(num_0: int = N_INITIAL, num_b: int = N_BOUNDARY, num_r: int = N_RESIDUAL, t_min = T_MIN, t_max = T_MAX, x_min = X_MIN, x_max = X_MAX):
-
-	#tf.keras.backend.set_floatx(DTYPE)
-
-	#t_lb = tf.constant(t_min, dtype = DTYPE)
-	#t_ub = tf.constant(t_max, dtype = DTYPE)
-	#x_lb = tf.constant(x_min, dtype = DTYPE)
-	#x_ub = tf.constant(x_max, dtype = DTYPE)
-
+def generate_data(num_0: int = N_INITIAL, num_b: int = N_BOUNDARY, num_r: int = N_RESIDUAL, bounds = get_bounds()):
+	t_min, t_max, x_min, x_max = bounds
 
 	# sampling initial boundary points
 	t_0 = np.ones((num_0, 1)) * t_min
@@ -52,6 +45,8 @@ def generate_data(num_0: int = N_INITIAL, num_b: int = N_BOUNDARY, num_r: int = 
 	# sampling collocation points
 	t_r = np.random.uniform(t_min, t_max, num_r)
 	x_r = np.random.uniform(x_min, x_max, num_r)
+
+	return t_0, x_0, u_0, t_b, x_b, u_b, t_r, x_r
 
 
 

model/FKPP-model.py
@@ -5,35 +5,44 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import imread
 import scipy
 import pandas as pd
-
-
 import time
 
-DTYPE = 'float32'
+class ScaleLayer(tf.keras.layers.Layer):
+    def __init__(self, scale_function = lambda x : x):
+    	super(ScaleLayer, self).__init__()
+      	self.scale = scale_function
+
+    def call(self, inputs):
+      	return self.scale(inputs)
+
 
 class PINN_FisherKPP:
 	def __init__(self, data, bounds, hidden_layers):
 		t_0, x_0, u_0, t_b, x_b, u_b, t_r, x_r = data
 		self.t_min, self.t_max, self.x_min, self.x_max = bounds
 
-		self.x_0 = tf.Tensor(x_0, dtype = DTYPE)
-		self.u_0 = tf.Tensor(u_0, dtype = DTYPE)
-		self.t_0 = tf.Tensor(t_0, dtype = DTYPE)
-		self.x_b = tf.Tensor(x_b, dtype = DTYPE)
-		self.u_b = tf.Tensor(u_b, dtype = DTYPE)
-		self.x_r = tf.Tensor(x_r, dtype = DTYPE)
-		self.t_r = tf.Tensor(t_r, dtype = DTYPE)
+		self.x_0 = tf.Tensor(x_0, dtype = tf.float32)
+		self.u_0 = tf.Tensor(u_0, dtype = tf.float32)
+		self.t_0 = tf.Tensor(t_0, dtype = tf.float32)
+		self.x_b = tf.Tensor(x_b, dtype = tf.float32)
+		self.u_b = tf.Tensor(u_b, dtype = tf.float32)
+		self.x_r = tf.Tensor(x_r, dtype = tf.float32)
+		self.t_r = tf.Tensor(t_r, dtype = tf.float32)
 		
 		self.hidden_layers = hidden_layers
 
 	def residual(t, x, u, u_t, u_x, u_xx):
 		return u_t - u_xx - u * (1 - u)
 
+
 	def build_model(self):
 
 		model = tf.keras.Sequential()
 		# input layer (t, x)
-		model.add(tf.keras.Input(shape = 2))
+		model.add(tf.keras.layers.InputLayer(2))
+		# scale inputs within bounds
+		scale_function = lambda inputs : 2 * (inputs - tf.Tensor([t_min, x_min])) / tf.Tensor([t_max - t_min, x_max - x_min]) - 1.0
+		model.add(ScaleLayer(scale_function = scale_function))
 
 		# hidden layers
 		for layer in self.hidden_layers:
@@ -43,7 +52,7 @@ class PINN_FisherKPP:
 				activation = 'tanh', 
 				kernel_initializer = 'glorot_normal'))
 
-		# output layer
+		# output layer with sigmoid activation
 		model.add(tf.keras.layers.Dense(1, activation = 'sigmoid'))
 
 		return model
@@ -107,12 +116,14 @@ class PINN_FisherKPP:
 			optimizer.apply_gradients(zip(grad, model.trainable_variables))
 			losses.append(loss.numpy())
 			if epoch % 10 == 0:
-				print("Epoch {}: loss = {}, time elapsed".format(epoch, loss.numpy(), time.time() - start_time))
+				print("Epoch {}: loss = {}, time elapsed".format(
+					epoch, loss.numpy(), time.time() - start_time))
 
 		# plot losses
 
 
-
+if __name__ == "__main__":
+	hidden_layers = [20, 20, 20, 20, 20, 20, 20, 20]
 
 
 