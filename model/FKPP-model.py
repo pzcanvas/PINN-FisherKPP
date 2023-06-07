cad1a22a43677ed26190cedc8dff4b4f57f34df9
Zhiqi <zl2@williams.edu>
2023-06-06 22:57:03 -0700

    Added support for adaptive re-sampling

--- model/FKPP-model.py
+++ model/FKPP-model.py
@@ -8,7 +8,9 @@ import scipy
 #import pandas as pd
 import time
 import data
+import os
 
+#tf.keras.backend.set_floatx('float32')
 
 # define a new tf.keras.layers.Layer per recommendation of 
 # TensorFlow workflow (instead of using tf.keras.layers.Lambda)
@@ -22,8 +24,8 @@ class ScaleLayer(tf.keras.layers.Layer):
 
 
 class PINN_FisherKPP:
-    def __init__(self, data, hidden_layers, learning_rate = 0.01, lr_decay = 'piecewise', activation = None):
-        t_0, x_0, u_0, t_b, x_b, u_b, t_r, x_r, bounds = data
+    def __init__(self, my_data, hidden_layers, learning_rate = 0.01, lr_decay = None, activation = 'relu', num_epochs = 200, adaptive = False):
+        t_0, x_0, u_0, t_b, x_b, u_b, t_r, x_r, bounds = my_data
         self.t_min, self.t_max, self.x_min, self.x_max = bounds
 
         self.x_0 = tf.convert_to_tensor(x_0, dtype = tf.float32)
@@ -34,16 +36,21 @@ class PINN_FisherKPP:
         self.t_b = tf.convert_to_tensor(t_b, dtype = tf.float32)
         self.x_r = tf.convert_to_tensor(x_r, dtype = tf.float32)
         self.t_r = tf.convert_to_tensor(t_r, dtype = tf.float32)
+        self.bounds = bounds
 
         
         self.hidden_layers = hidden_layers
         self.activation = activation
         self.lr_decay = lr_decay
         self.learning_rate = learning_rate
+        self.num_epochs = num_epochs
+        self.adaptive = adaptive
 
     def residual(self, t, x, u, u_t, u_x, u_xx):
-        return u_t - u_xx - u * (1 - u)
+        return u_t - u_xx - 6 * u * (1 - u)
 
+    def u_exact(self, t, x):
+        return 1 / ((1 + np.exp(x - 5 * t)) ** 2)
 
     def build_model(self):
 
@@ -51,7 +58,7 @@ class PINN_FisherKPP:
         # input layer (t, x)
         model.add(tf.keras.layers.InputLayer(2))
         # scale inputs within bounds
-        scale_function = lambda inputs : 2 * (inputs - tf.convert_to_tensor([self.t_min, self.x_min])) / tf.convert_to_tensor([self.t_max - self.t_min, self.x_max - self.x_min]) - 1.0
+        scale_function = lambda inputs : 2 * (inputs - tf.convert_to_tensor([self.t_min, self.x_min], dtype = tf.float32)) / tf.convert_to_tensor([self.t_max - self.t_min, self.x_max - self.x_min], dtype = tf.float32) - 1.0
         model.add(ScaleLayer(scale_function = scale_function))
 
         # hidden layers
@@ -67,23 +74,23 @@ class PINN_FisherKPP:
 
         return model
 
-
-    def evaluate_residual(self, model):
+    def evaluate_residual(self, model, t, x):
 
         with tf.GradientTape(persistent = True) as tape:
-            t = self.t_r
-            x = self.x_r
+            #t = self.t_r
+            #x = self.x_r
             tape.watch(t)
             tape.watch(x)
+
             u_r = model(tf.stack([t, x], axis = 1))
             u_x = tape.gradient(u_r, x)
-        #u_x = tape.gradient(u_r, x)
+
         u_t = tape.gradient(u_r, t)
         u_xx = tape.gradient(u_x, x)
 
         del tape
-
-        return self.residual(t, x, u_r, u_t, u_x, u_xx)
+        res = self.residual(t, x, u_r, u_t, u_x, u_xx)
+        return res
 
     def compute_loss(self, model, scale_initial_loss = None):
         loss = 0
@@ -95,13 +102,13 @@ class PINN_FisherKPP:
         u_b_pred = model(tf.stack([self.t_b, self.x_b], axis = 1))
         loss += tf.reduce_mean(tf.square(self.u_b - u_b_pred))
 
-        res = self.evaluate_residual(model)
+        res = self.evaluate_residual(model, self.t_r, self.x_r)
         loss += tf.reduce_mean(tf.square(res))
 
         return loss
     #@tf.function
     def compute_grad(self, model):
-        with tf.GradientTape() as tape:
+        with tf.GradientTape(persistent = True) as tape:
             tape.watch(model.trainable_variables)
             loss = self.compute_loss(model)
         grad = tape.gradient(loss, model.trainable_variables)
@@ -118,7 +125,7 @@ class PINN_FisherKPP:
         if self.lr_decay == 'piecewise':
             learning_rate_fn = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
                 [num_epochs//3, 2*num_epochs//3], 
-                [self.learning_rate, self.learning_rate / 10, self.learning_rate / 20])
+                [self.learning_rate, self.learning_rate / 10.0, self.learning_rate / 20.0])
 
         if self.lr_decay == 'exponential':
             learning_rate_fn = tf.keras.optimizers.schedules.ExponentialDecay(
@@ -133,9 +140,23 @@ class PINN_FisherKPP:
             loss, grad = self.compute_grad(model)
             optimizer.apply_gradients(zip(grad, model.trainable_variables))
             losses.append(loss.numpy())
-            if epoch % 10 == 0:
+            if epoch % 50 == 0:
                 print("Epoch {}: loss = {}, time elapsed {}".format(
                     epoch, loss.numpy(), time.time() - start_time))
+                if self.adaptive:
+                    #self.x_r = self.x_r[:-20]
+                    #self.t_r = self.t_r[:-20]
+                    test_t, test_x = data.sample_collocation(100, self.bounds)
+                    test_t = tf.convert_to_tensor(test_t, dtype = tf.float32)
+                    test_x = tf.convert_to_tensor(test_x, dtype = tf.float32)
+                    res = self.evaluate_residual(model, test_t, test_x)
+                    idx = tf.math.top_k(tf.reshape(res,[-1]), k = 20).indices
+                    new_t = tf.gather(test_t, idx)
+                    new_x = tf.gather(test_x, idx)
+                    self.t_r = tf.concat([self.t_r, new_t], 0)
+                    self.x_r = tf.concat([self.x_r, new_x], 0)
+
+
 
         return model, losses
 
@@ -143,16 +164,16 @@ class PINN_FisherKPP:
         pass
 
 
-    def plot_loss(self, losses):
+    def plot_loss(self, losses, subfolder):
         fig = plt.figure()
         hist = fig.add_subplot(111)
         hist.plot(range(0, len(losses)), losses)
         hist.set_xlabel('# of epochs')
         hist.set_ylabel('loss')
-        plt.savefig('loss_plot.png', bbox_inches = 'tight')
+        plt.savefig(subfolder + 'loss_plot.png', bbox_inches = 'tight')
 
     # this plot function is based on existing pyplot tutorials from Github 
-    def plot_prediction(self, model):
+    def plot_prediction(self, model, subfolder):
         # Set up meshgrid
         N = 600
         t_dom = np.linspace(self.t_min, self.t_max, N + 1)
@@ -175,26 +196,53 @@ class PINN_FisherKPP:
         ax.set_ylabel('$x$')
         ax.set_zlabel('$u_\\theta(t,x)$')
         ax.set_title('Solution of FKPP equation');
-        plt.savefig('FKPP_Solution.pdf', bbox_inches='tight', dpi=300)
+        plt.savefig(subfolder + 'FKPP_Solution.pdf', bbox_inches='tight', dpi=300)
 
-    def train_adaptive(self, num_epochs):
-        pass
+        # exact solution
+        u_true = self.u_exact(t_grid, x_grid)
+        l2_error = np.linspace.norm(u_true - u_predict)
+        print("L2 error with exact solution: {}".format(l2_error))
 
-    def train_and_show_results(self, num_epochs = 100, adaptive = False):
-        if not adaptive: 
-            model, losses = self.train(num_epochs)
-            self.plot_loss(losses)
-            self.plot_prediction(model)
-        if adaptive:
-            pass
+
+
+    def train_and_show_results(self, subfolder):
+        model, losses = self.train(self.num_epochs)
+        self.plot_loss(losses, subfolder)
+        self.plot_prediction(model, subfolder)
+        return model, losses
+
+
+
+    def test_accuracy(self, model, case):
+        N = 10
+        M = 100
+        tspace = np.linspace(lb[0], ub[0], M + 1)
+        xspace = np.linspace(lb[1], ub[1], N + 1)
+
+        u_pred = model(tspace, xspace)
+
+        if case == 1:
+            u_true = u_exact(tspace, xspace)
+            return np.linspace.norm(u_pred - u_true)
+        else:
+            return None
 
 
 
 if __name__ == "__main__":
-    hidden_layers = [20, 20, 20, 20, 20, 20, 20, 20]
-    data = data.generate_data(data.u_0, data.u_b)
-    my_PINN = PINN_FisherKPP(data, hidden_layers)
-    my_PINN.train_and_show_results()
+    my_path = os.path.dirname(__file__)
+    hidden_layers = [20 for i in range(10)]
+    num_epochs = 2000
+
+
+    my_data_case1 = data.generate_data(data.u_0_case1, data.u_b_case1)
+    my_PINN_case1 = PINN_FisherKPP(my_data_case1, hidden_layers, num_epochs = num_epochs, adaptive = True)
+    case1_path = os.path.join(my_path, 'case1/')
+    os.makedirs(case1_path)
+    my_PINN_case1.train_and_show_results(case1_path)
+    #print("L2 error: ")
+    #print(my_PINN_case1.test_accuracy(model_case1, case = 1))
+
 
 
 
--- model/data.py
+++ model/data.py
@@ -19,34 +19,50 @@ X_MAX = 1.
 # initial conditions
 ''' 
 '''
-def u_0(x):
-	return 1 / (1 + np.exp(x)) ** 2
+def u_0_case1(x):
+    return 1 / (1 + np.exp(x)) ** 2
 
 # boundary conditions
-def u_b(t, x):
-	return 1 / (1 + np.exp(x - 5 * t)) ** 2
+def u_b_case1(t, x):
+    return 1 / (1 + np.exp(x - 5 * t)) ** 2
+
 
 def get_bounds(t_min = T_MIN, t_max = T_MAX, x_min = X_MIN, x_max = X_MAX):
-	return t_min, t_max, x_min, x_max
+    return t_min, t_max, x_min, x_max
 
-def generate_data(u_0, u_b, num_0: int = N_INITIAL, num_b: int = N_BOUNDARY, num_r: int = N_RESIDUAL, bounds = get_bounds()):
-	t_min, t_max, x_min, x_max = bounds
+def sample_initial(func_u_0, num_0, bounds):
+    t_min, t_max, x_min, x_max = bounds
 
-	# sampling initial boundary points
-	t_0 = np.ones(num_0) * t_min
-	x_0 = np.random.uniform(x_min, x_max, num_0)
-	u_0 = u_0(x_0)
+    # sampling initial points
+    t_0 = np.ones((num_0, 1)) * t_min
+    x_0 = np.random.uniform(x_min, x_max, (num_0, 1))
+    u_0 = func_u_0(x_0)
+    return (t_0, x_0, u_0)
 
-	# sampling boundary points
-	t_b = np.random.uniform(t_min, t_max, num_b)
-	x_b = np.random.choice([x_min, x_max], size = num_b)
-	u_b = u_b(t_b, x_b)
+def sample_boundary(func_u_b, num_b, bounds):
+    t_min, t_max, x_min, x_max = bounds
 
-	# sampling collocation points
-	t_r = np.random.uniform(t_min, t_max, num_r)
-	x_r = np.random.uniform(x_min, x_max, num_r)
+    # sampling boundary points
+    t_b = np.random.uniform(t_min, t_max, (num_b, 1))
+    x_b = np.random.choice([x_min, x_max], (num_b, 1))
+    u_b = func_u_b(t_b, x_b)
+    return (t_b, x_b, u_b)
 
-	return t_0, x_0, u_0, t_b, x_b, u_b, t_r, x_r, bounds
+def sample_collocation(num_r, bounds):
+    t_min, t_max, x_min, x_max = bounds
+    # sampling collocation points
+    t_r = np.random.uniform(t_min, t_max, (num_r, 1))
+    x_r = np.random.uniform(x_min, x_max, (num_r, 1))
+    return (t_r, x_r)
+
+
+def generate_data(func_u_0, func_u_b, num_0: int = N_INITIAL, num_b: int = N_BOUNDARY, num_r: int = N_RESIDUAL):
+    bounds = get_bounds()
+    t_0, x_0, u_0 = sample_initial(func_u_0, num_0, bounds)
+    t_b, x_b, u_b = sample_boundary(func_u_b, num_b, bounds)
+    t_r, x_r = sample_collocation(num_r, bounds)
+
+    return t_0, x_0, u_0, t_b, x_b, u_b, t_r, x_r, bounds
 
 
 
